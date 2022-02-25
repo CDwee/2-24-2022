229da812a7318462fa02ce2364715a7e092e8523
CDwee <96454637+CDwee@users.noreply.github.com>
2022-02-24 18:49:39 -0600

    Update and rename README.md to script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+// Strated at 5:44 2-24-2022
+
+btnPark1.addEventListener('click', function () {  
+if (park === 1) {
+    console.log("YOU WON!");
+    bodyEl();
+}
+});
+
+btnPark2.addEventListener('click', function () {  
+if (park === 2) {
+    console.log("YOU WON!");
+    bodyEl();
+}});
+
+btnPark3.addEventListener('click', function () {  
+if (park === 3) {
+    console.log("YOU WON!");
+    bodyEl();
+}});
+
+btnPark4.addEventListener('click', function () {  
+if (park === 4) {
+    console.log("YOU WON!");
+    bodyEl();
+}});
+
+btnPark5.addEventListener('click', function () {  
+if (park === 5) {
+    console.log("YOU WON!");
+    bodyEl();
+}});
+    
+btnPark6.addEventListener('click', function () {  
+if (park === 6) {
+    console.log("YOU WON!");
+    bodyEl();
+}});
+
+// Made my game display in the console "YOU WON!" but need to figure out how to make the whole background color change when guessed the correct park.
+
+// Ended at 6:48 2-24-2022
